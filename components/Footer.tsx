ae50344c12250250af064c2ecfd781195df6ba85
nirvaghani008 <nirvaghani008@gmail.com>
2026-08-13 12:17:24 +0530

    update ToolBit badge secret token to 9d725f89c0a56ce8f51b08284339b1027118c72ec55b74b4 and use light theme SVG

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -47,11 +47,11 @@ export default function Footer() {
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{ cursor: "pointer" }}
-                data-tb-secret="0bd34f603faf697f4f16a5b1b4fd56e39e7197e72ed84f58"
+                data-tb-secret="9d725f89c0a56ce8f51b08284339b1027118c72ec55b74b4"
               >
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
-                  src="https://cdn.toolbit.ai/external-share-img/dark-featured.svg"
+                  src="https://cdn.toolbit.ai/external-share-img/light-featured.svg"
                   alt="Featured on ToolBit.ai - ToonForge AI"
                   style={{ display: "block", width: "250px", height: "76px" }}
                   width="250"
